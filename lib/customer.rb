813e66235b6bd30055a76cab16f81c2464bef22b
Eric <eric.m.lombardo@gmail.com>
2020-12-07 13:07:44 -0500

    commit tree 9f335169659823ecbd6e6bb36ed78c7ae5518252

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = []  # collects all instances of customer
+  def initialize(name, age)
+    @name = name  # starts with name and age
+    @age = age
+    @@all << self # pushes all instance into @@all array
+  end 
+
+  def self.all  # getter method for @@all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip) # new meal with waiter, customer, total and tip linked
+    meal = Meal.new(waiter, self, total, tip)
+  end
+
+  def meals #=> Array of all customer's meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters # array of all waiters customer has had
+    meals.map {|name| name.waiter}
+  end
+
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,16 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
 
+  @@all = []  # collects all meals
+  def initialize(waiter, customer, total, tip = 0)
+    @waiter = waiter  # starts with waiter, customer, total and tip 
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self # pushes all meal instances into @@all
+  end
+
+  def self.all  # getter method for @@all
+    @@all
+  end
 end
